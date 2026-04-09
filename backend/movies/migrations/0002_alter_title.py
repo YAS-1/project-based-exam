d1c03932e47e4ba3f954389b9682c3c52796e7e5
Andrea-Mwonge <andreamwonge700@gmail.com>
2026-04-09 20:48:58 +0300

    Completed backend setup and exam tasks

---
+++ backend/movies/migrations/0002_alter_title.py
@@ -0,0 +1,14 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("movies", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="movie",
+            name="title",
+            field=models.CharField(max_length=255, unique=True),
+        ),
+    ]
